2b63a12cd4dbf6b8c36b111b3d79d23b99779665
szvmczek <szymon.lebensztejn.essa@gmail.com>
2025-12-14 13:06:08 +0100

    added prod and dev

--- build-and-restart.bat
+++ build-and-restart.bat
@@ -1,5 +1,4 @@
-
 docker build . -t ogloszex
 docker stop ogloszex || true
 docker rm ogloszex || true
-docker run -d -p 8080:8080 --name ogloszex -e SPRING_PROFILES_ACTIVE=prod ogloszex
+docker run -d -p 8080:8080 --name=ogloszex -e SPRING_PROFILES_ACTIVE=prod --network ogloszex-network --restart unless-stopped ogloszex
